e029c66ba3ac00e9d1a17bd2c07729a7642f68ba
Nikita Maslo <nikitamalco203@gmail.com>
2026-08-17 16:05:31 +0300

    Add JetHub D3-mini (j310) board support
    Amlogic S905Y5 (s7) with the JetHome vendor 5.15 kernel and u-boot fork.
    Board carries an A/B recovery layout in eMMC, so the rootfs partition starts
    at 336 MiB, leaving two 102 MiB raw slots ahead of it; j100 uses the same
    layout. FIP blobs for all JetHub boards now come from a single fork.

--- config/bootscripts/boot-jethub.cmd
+++ config/bootscripts/boot-jethub.cmd
@@ -4,7 +4,7 @@
 #
 
 setenv scriptaddr      "0x12000000"
-setenv kernel_addr_r  "0x14000000"
+setenv kernel_addr_r  "0x08200000"
 setenv ramdisk_addr_r "0x1A000000"
 setenv fdt_addr_r     "0x10800000"
 setenv overlay_error "false"
@@ -16,25 +16,60 @@ setenv rootfstype "ext4"
 setenv docker_optimizations "on"
 setenv prefix "boot/"
 
+# Debug: show partition table
+echo "=== Boot device partition table ==="
+mmc dev ${devnum}
+mmc part
+
 # Show what uboot default fdtfile is
 echo "U-boot default fdtfile: ${fdtfile}"
 echo "Current variant: ${variant}"
 
+# Set boot partition device spec
+if test -z "${rootpart}"; then
+	setenv rootpart "1"
+fi
+setenv bootdev "${devnum}:${rootpart}"
+
 # legacy kernel values from armbianEnv.txt
-if test -e ${devtype} ${devnum} ${prefix}armbianEnv.txt; then
-	load ${devtype} ${devnum} ${scriptaddr} ${prefix}armbianEnv.txt
+if test -e ${devtype} ${bootdev} ${prefix}armbianEnv.txt; then
+	load ${devtype} ${bootdev} ${scriptaddr} ${prefix}armbianEnv.txt
 	env import -t ${scriptaddr} ${filesize}
 fi
 
-# get PARTUUID of first partition on SD/eMMC it was loaded from
-# mmc 0 is always mapped to device u-boot (2016.09+) was loaded from
-if test "${devtype}" = "mmc"; then part uuid mmc ${devnum}:1 partuuid; fi
+setenv verbosity "7"
+
+# Get partition UUID for root device
+if test "${devtype}" = "mmc"; then
+	part uuid mmc ${devnum}:${rootpart} partuuid
+fi
+
+# Determine root device based on boot device
+if test -n "${partuuid}"; then
+    setenv rootdev "PARTUUID=${partuuid}"
+fi
+echo "Boot device: mmc ${devnum}, Root device: ${rootdev}"
 
 echo "Current fdtfile after armbianEnv: ${fdtfile}"
 
-if test "${console}" = "serial"; then setenv consoleargs "console=ttyAML0,115200n8"; fi
+# Set fdtfile for J310 if not set
+if test "$board" = "jethub_j310"; then
+    if test -z "${fdtfile}"; then
+        setenv fdtfile "amlogic/meson-s7-jethub-j310.dtb"
+        echo "Set fdtfile for J310: ${fdtfile}"
+    fi
+fi
 
-setenv bootargs "root=${rootdev} rootwait rootflags=data=writeback rootfstype=${rootfstype} ${consoleargs} no_console_suspend consoleblank=0 coherent_pool=2M loglevel=${verbosity} fsck.mode=force fsck.repair=yes net.ifnames=0 ${extraargs} ${extraboardargs}"
+# Set console based on board type
+if test "${console}" = "serial"; then
+    if test "$board" = "jethub_j310"; then
+        setenv consoleargs "console=ttyS0,921600n8 earlycon=aml_uart,0xfe07a000"
+    else
+        setenv consoleargs "console=ttyAML0,115200n8"
+    fi
+fi
+
+setenv bootargs "root=${rootdev} rootwait rootflags=data=writeback rootfstype=${rootfstype} ${consoleargs} no_console_suspend consoleblank=0 coherent_pool=16M loglevel=${verbosity} fsck.mode=force fsck.repair=yes net.ifnames=0 ${extraargs} ${extraboardargs}"
 
 if test -n "${board_name}"; then setenv bootargs "${bootargs} board=${board}"; fi
 
@@ -64,22 +99,21 @@ if test "$board" = "jethub-j100"; then
 fi;
 
 
-load ${devtype} ${devnum} ${ramdisk_addr_r} ${prefix}uInitrd
-load ${devtype} ${devnum} ${kernel_addr_r} ${prefix}Image
-load ${devtype} ${devnum} ${fdt_addr_r} ${prefix}dtb/${fdtfile}
+load ${devtype} ${bootdev} ${ramdisk_addr_r} ${prefix}uInitrd
+load ${devtype} ${bootdev} ${kernel_addr_r} ${prefix}Image
+load ${devtype} ${bootdev} ${fdt_addr_r} ${prefix}dtb/${fdtfile}
 fdt addr ${fdt_addr_r}
 fdt resize 65536
 
-
 for overlay_file in ${overlays}; do
-	if load ${devtype} ${devnum} ${scriptaddr} ${prefix}dtb/amlogic/overlay/${overlay_prefix}-${overlay_file}.dtbo; then
+	if load ${devtype} ${bootdev} ${scriptaddr} ${prefix}dtb/amlogic/overlay/${overlay_prefix}-${overlay_file}.dtbo; then
 		echo "Applying kernel provided DT overlay ${overlay_prefix}-${overlay_file}.dtbo"
 		fdt apply ${scriptaddr} || setenv overlay_error "true"
 	fi
 done
 
 for overlay_file in ${user_overlays}; do
-	if load ${devtype} ${devnum} ${scriptaddr} ${prefix}overlay-user/${overlay_file}.dtbo; then
+	if load ${devtype} ${bootdev} ${scriptaddr} ${prefix}overlay-user/${overlay_file}.dtbo; then
 		echo "Applying user provided DT overlay ${overlay_file}.dtbo"
 		fdt apply ${scriptaddr} || setenv overlay_error "true"
 	fi
@@ -87,14 +121,14 @@ done
 
 if test "${overlay_error}" = "true"; then
 	echo "Error applying DT overlays, restoring original DT"
-	load ${devtype} ${devnum} ${fdt_addr_r} ${prefix}dtb/${fdtfile}
+	load ${devtype} ${bootdev} ${fdt_addr_r} ${prefix}dtb/${fdtfile}
 else
-	if load ${devtype} ${devnum} ${scriptaddr} ${prefix}dtb/amlogic/overlay/${overlay_prefix}-fixup.scr; then
+	if load ${devtype} ${bootdev} ${scriptaddr} ${prefix}dtb/amlogic/overlay/${overlay_prefix}-fixup.scr; then
 		echo "Applying kernel provided DT fixup script (${overlay_prefix}-fixup.scr)"
 		source ${scriptaddr}
 	fi
-	if test -e ${devtype} ${devnum} ${prefix}fixup.scr; then
-		load ${devtype} ${devnum} ${scriptaddr} ${prefix}fixup.scr
+	if test -e ${devtype} ${bootdev} ${prefix}fixup.scr; then
+		load ${devtype} ${bootdev} ${scriptaddr} ${prefix}fixup.scr
 		echo "Applying user provided fixup script (fixup.scr)"
 		source ${scriptaddr}
 	fi
